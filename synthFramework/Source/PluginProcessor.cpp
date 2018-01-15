67c7c9fd567fce3f0557184ff76d781ac4cd4810
Troy Kurniawan <troy.kurniawan@sjsu.edu>
2018-01-15 00:23:38 -0800

    Initial Push
    The foundation of the synthesizer plugin is laid out.

---
+++ synthFramework/Source/PluginProcessor.cpp
@@ -0,0 +1,196 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#include "PluginProcessor.h"
+#include "PluginEditor.h"
+
+
+//==============================================================================
+SynthFrameworkAudioProcessor::SynthFrameworkAudioProcessor()
+#ifndef JucePlugin_PreferredChannelConfigurations
+     : AudioProcessor (BusesProperties()
+                     #if ! JucePlugin_IsMidiEffect
+                      #if ! JucePlugin_IsSynth
+                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
+                      #endif
+                       .withOutput ("Output", AudioChannelSet::stereo(), true)
+                     #endif
+                       )
+#endif
+{
+    // When we press a note, we don't want previous note presses to interfere with the next note.
+    mySynth.clearVoices();
+    
+    // Here, the 5 represents how many maximum voices the synth is capable of simulatenously.
+    // In other words, the synth is capable of playing 5 notes (voices) at the same time.
+    // Change 5 to any number you want.
+    for(int i=0; i<5; i++)
+    {
+        mySynth.addVoice(new SynthVoice());
+    }
+    
+    
+    mySynth.clearSounds();
+    mySynth.addSound(new SynthSound());
+    
+}
+
+SynthFrameworkAudioProcessor::~SynthFrameworkAudioProcessor()
+{
+}
+
+//==============================================================================
+const String SynthFrameworkAudioProcessor::getName() const
+{
+    return JucePlugin_Name;
+}
+
+bool SynthFrameworkAudioProcessor::acceptsMidi() const
+{
+   #if JucePlugin_WantsMidiInput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SynthFrameworkAudioProcessor::producesMidi() const
+{
+   #if JucePlugin_ProducesMidiOutput
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+bool SynthFrameworkAudioProcessor::isMidiEffect() const
+{
+   #if JucePlugin_IsMidiEffect
+    return true;
+   #else
+    return false;
+   #endif
+}
+
+double SynthFrameworkAudioProcessor::getTailLengthSeconds() const
+{
+    return 0.0;
+}
+
+int SynthFrameworkAudioProcessor::getNumPrograms()
+{
+    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
+                // so this should be at least 1, even if you're not really implementing programs.
+}
+
+int SynthFrameworkAudioProcessor::getCurrentProgram()
+{
+    return 0;
+}
+
+void SynthFrameworkAudioProcessor::setCurrentProgram (int index)
+{
+}
+
+const String SynthFrameworkAudioProcessor::getProgramName (int index)
+{
+    return {};
+}
+
+void SynthFrameworkAudioProcessor::changeProgramName (int index, const String& newName)
+{
+}
+
+//==============================================================================
+void SynthFrameworkAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
+{
+    // Use this method as the place to do any pre-playback
+    // initialisation that you need..
+    
+    // Clear out unused samples from the last key pressed
+    ignoreUnused(samplesPerBlock);
+    
+    
+    lastSampleRate = sampleRate;
+    mySynth.setCurrentPlaybackSampleRate(lastSampleRate);
+}
+
+void SynthFrameworkAudioProcessor::releaseResources()
+{
+    // When playback stops, you can use this as an opportunity to free up any
+    // spare memory, etc.
+}
+
+#ifndef JucePlugin_PreferredChannelConfigurations
+bool SynthFrameworkAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
+{
+  #if JucePlugin_IsMidiEffect
+    ignoreUnused (layouts);
+    return true;
+  #else
+    // This is the place where you check if the layout is supported.
+    // In this template code we only support mono or stereo.
+    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
+     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
+        return false;
+
+    // This checks if the input layout matches the output layout
+   #if ! JucePlugin_IsSynth
+    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
+        return false;
+   #endif
+
+    return true;
+  #endif
+}
+#endif
+
+void SynthFrameworkAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
+{
+    
+    ScopedNoDenormals noDenormals;
+    
+    buffer.clear();
+    
+    mySynth.renderNextBlock(buffer, midiMessages, 0, buffer.getNumSamples());
+    
+}
+
+//==============================================================================
+bool SynthFrameworkAudioProcessor::hasEditor() const
+{
+    return true; // (change this to false if you choose to not supply an editor)
+}
+
+AudioProcessorEditor* SynthFrameworkAudioProcessor::createEditor()
+{
+    return new SynthFrameworkAudioProcessorEditor (*this);
+}
+
+//==============================================================================
+void SynthFrameworkAudioProcessor::getStateInformation (MemoryBlock& destData)
+{
+    // You should use this method to store your parameters in the memory block.
+    // You could do that either as raw data, or use the XML or ValueTree classes
+    // as intermediaries to make it easy to save and load complex data.
+}
+
+void SynthFrameworkAudioProcessor::setStateInformation (const void* data, int sizeInBytes)
+{
+    // You should use this method to restore your parameters from this memory block,
+    // whose contents will have been created by the getStateInformation() call.
+}
+
+//==============================================================================
+// This creates new instances of the plugin..
+AudioProcessor* JUCE_CALLTYPE createPluginFilter()
+{
+    return new SynthFrameworkAudioProcessor();
+}
